a7f4c92e598e5ccfc372bff87017060b4e7ffd27
Gander Simon <siga241@gmail.com>
2026-03-06 11:19:23 +0100

    Remove obsolete _scripts folder

--- _scripts/generate_settings.py
+++
@@ -1,8 +0,0 @@
-settings_path = "../_config/settings.json"
-
-
-from _lib.settings_structure.settings_structure import save_settings, Settings
-
-settings = Settings()
-save_settings(settings_path, settings)
-
